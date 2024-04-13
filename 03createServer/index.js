92d9c55f90a28b29ce3ba757ecf1f1fbacf7cb3e
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-04-13 18:00:36 +0530

    creating a basic server

--- 03createServer/index.js
+++ 03createServer/index.js
@@ -1,2 +1,26 @@
+const http = require("http");
+const fs = require("fs");
 
-const http=require('http');
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()} :${req.url} New Request Received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("You are on home Page");
+        break;
+
+      case "/about":
+        res.end("Hello I'm Srinibash");
+        break;
+
+      case "/help":
+        res.end("help line number - +91-6372773960");
+        break;
+
+      default:
+        res.end("404 not found");
+    }
+  });
+});
+
+myServer.listen(8000, () => console.log("Server Started"));
