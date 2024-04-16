986855e1ee8fac76e65639c2dcb6f17eb774333a
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-04-16 10:48:28 +0530

    matering http methods

--- 03createServer/index.js
+++ 03createServer/index.js
@@ -1,16 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+
+  const myUrl = url.parse(req.url, true); //used url npm library // by parsing true it return search query object
+  console.log(myUrl);
   const log = `${Date.now()} :${req.url} New Request Received\n`;
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("You are on home Page");
         break;
 
       case "/about":
-        res.end("Hello I'm Srinibash");
+        res.end(`Hello I'm ${myUrl.query.user_name}`);
         break;
 
       case "/help":
