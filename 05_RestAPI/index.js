1453836d22934ef6af8af5e322c0d06079efdaca
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-04-27 20:58:17 +0530

    resolved minor typo on 05 project

--- 05_RestAPI/index.js
+++ 05_RestAPI/index.js
@@ -5,6 +5,36 @@ const fs = require("fs");
 const app = express();
 const port = 8000;
 
+//Middleware concepts:
+app.use(express.urlencoded({ extended: false })); //->* this middleware helps to convert frontend data to object and put into the req.body
+
+app.use((req, res, next) => {
+  console.log("Hello from Middleware: 1");
+  req.myUserName = "srinibash"; //this assigned data can access in the next middle ware or APIs
+  // res.send({ message: "Verify your details" }); // it stops the API request here
+  next(); // calling next middleware
+});
+
+app.use((req, res, next) => {
+  console.log("hello from middleware: 2", req.myUserName);
+  // res.end("hey you stop in middleware 2");
+  next();
+});
+
+//creating logs by using middle wares
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `\n${Date.now()} : ${req.ip} : ${req.method} : ${req.path}`,
+    (err, dat) => {
+      next();
+    }
+  );
+});
+
+
+
+
 //hybrid APIs
 app.get("/users", (req, res) => {
   const html = `
@@ -19,6 +49,8 @@ app.get("/users", (req, res) => {
 
 //REST APIs -> check out task.txt page for detailing of REST APIs
 app.get("/api/users", (req, res) => {
+  res.setHeader("X-MyName","A Srinibash Achary"); //Custom header
+  //for good practice for build custom header always append 'X-' before it like 52 line
   return res.json(data);
 });
 
@@ -28,6 +60,7 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/api/users/:id", (req, res) => {
   const id = req.params.id;
   const user = data.find((user) => user.id == id);
+  if(!user) return res.status(404).json({message:"user not found"})
   if (user) {
     return res.json(user);
   } else {
@@ -37,6 +70,9 @@ app.get("/api/users/:id", (req, res) => {
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if(!body.first_name || !body.last_name || !body.email){
+    return res.status(201).json({message:"require all fields"})
+  }
   data.push({ id: data.length + 1, ...body });
   fs.writeFile("MOCK_DATA.json", JSON.stringify(data), (error, dat) => {
     return res.json({ id: data.length, status: "success" });
