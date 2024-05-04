9f577d9881de4b00fcb144b63c668df8fa996f4f
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-05-04 20:13:06 +0530

    Done with Server side rendering- Shortened url projects

--- 07_SortUrl/controller/url.js
+++ 07_SortUrl/controller/url.js
@@ -2,42 +2,43 @@ const shortid = require("shortid");
 const URL = require("../models/url");
 
 async function handelGenerateSortUrl(req, res) {
-  const shortID = shortid();
-  const body = req.body;
-  if (!body.url) return res.status(400).json({ error: "URL is required" });
-  await URL.create({
-    shortId: shortID,
-    redirectUrl: body.url,
-    visitHistory: [],
-  });
-  return res.json({ id: shortID });
+    const shortID = shortid();
+    const body = req.body;
+    if (!body.url) return res.status(400).json({error: "URL is required"});
+    await URL.create({
+        shortId: shortID,
+        redirectUrl: body.url,
+        visitHistory: [],
+    });
+    return res.render("home", {id: shortID});
 }
 
 async function handleRedirectUrl(req, res) {
-  const shortId = req.params.sortId;
-  const entry = await URL.findOneAndUpdate(
-    {
-      shortId,
-    },
-    {
-      $push: {
-        visitHistory: { timeStamps: Date.now() },
-      },
-    }
-  );
-  res.redirect(entry.redirectUrl);
+    const shortId = req.params.sortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push: {
+                visitHistory: {timeStamps: Date.now()},
+            },
+        }
+    );
+    res.redirect(entry.redirectUrl);
 }
+
 async function handleGetAnalytics(req, res) {
-  const shortId = req.params.shortId;
-  const result = await URL.findOne({ shortId });
-  return res.status(201).json({
-    totalClicks: result.visitHistory.length,
-    Analytics: result.visitHistory,
-  });
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId});
+    return res.status(201).json({
+        totalClicks: result.visitHistory.length,
+        Analytics: result.visitHistory,
+    });
 }
 
 module.exports = {
-  handelGenerateSortUrl,
-  handleRedirectUrl,
-  handleGetAnalytics,
+    handelGenerateSortUrl,
+    handleRedirectUrl,
+    handleGetAnalytics,
 };
--- 07_SortUrl/index.js
+++ 07_SortUrl/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./connect");
+const URL = require("./models/url");
+const path = require("path");
+const staticRouter = require("./routes/static");
 
 const app = express();
 const PORT = 8001;
@@ -8,9 +11,21 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short_url").then(() =>
   console.log("MongooDB Connected")
 );
 
-app.use(express.json());
-app.use("/url", urlRoute);
+//EJS configuration for the serverside redering
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views")); //view folder contains ejs files
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use("/url", urlRoute);
+app.use("/", staticRouter);
+
+app.get("/html", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("homeUrl", {
+    urls: allUrls,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started at PORT: ${PORT}`);
